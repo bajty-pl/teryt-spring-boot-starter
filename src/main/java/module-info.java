51467518de56c3f8f3c8da053c645adc3cab1c10
Mateusz Polak <mpolak90@gmail.com>
2026-05-24 12:34:06 +0200

    build: add `jakarta.xml.ws` module dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module teryt.spring.boot.starter {
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires jakarta.xml.bind;
+    requires jakarta.xml.ws;
 
     exports pl.bajty.teryt.api;
     exports pl.bajty.teryt.model;
