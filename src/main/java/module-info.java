4d7ffd654a24866eaef4885d0e24ad7a53c0ac4c
Mateusz Polak <mpolak90@gmail.com>
2026-05-24 12:34:54 +0200

    build: add `java.logging` module dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module teryt.spring.boot.starter {
     requires spring.boot.autoconfigure;
     requires jakarta.xml.bind;
     requires jakarta.xml.ws;
+    requires java.logging;
 
     exports pl.bajty.teryt.api;
     exports pl.bajty.teryt.model;
