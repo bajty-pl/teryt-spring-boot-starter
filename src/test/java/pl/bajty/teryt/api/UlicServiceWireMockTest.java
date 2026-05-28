af72c7f4a33339501a45c70bc2c20a6b1d8e3fbd
Mateusz Polak <mpolak90@gmail.com>
2026-05-28 21:13:58 +0200

    test: remove redundant `getSlownikCechULIC` test from `UlicServiceWireMockTest`

--- src/test/java/pl/bajty/teryt/api/UlicServiceWireMockTest.java
+++ src/test/java/pl/bajty/teryt/api/UlicServiceWireMockTest.java
@@ -3,7 +3,6 @@ package pl.bajty.teryt.api;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pl.bajty.teryt.model.dto.Simc;
-import pl.bajty.teryt.model.dto.Slownik;
 import pl.bajty.teryt.model.dto.Ulica;
 
 import java.time.LocalDate;
@@ -20,22 +19,6 @@ class UlicServiceWireMockTest extends AbstractTerytClientWireMockTest {
 
     private static final String ACTION_WYSZUKAJ_ULICE = "ITerytWs1/WyszukajUlice";
     private static final String RESPONSE_WYSZUKAJ_ULICE = "WyszukajUliceResponse.xml";
-
-    private static final String ACTION_POBIERZ_SLOWNIK_CECH_ULIC = "ITerytWs1/PobierzSlownikCechULIC";
-    private static final String RESPONSE_POBIERZ_SLOWNIK_CECH_ULIC = "PobierzSlownikCechULICResponse.xml";
-
-    @Test
-    @DisplayName("getSlownikCechULIC() zwraca listę cech")
-    void getSlownikCechULICShouldReturnList() {
-        stubSoapOk(ACTION_POBIERZ_SLOWNIK_CECH_ULIC, RESPONSE_POBIERZ_SLOWNIK_CECH_ULIC);
-
-        var cechy = terytClient.getSlownikCechULIC();
-
-        assertThat(cechy).isNotEmpty();
-        assertThat(cechy.stream().map(Slownik::getNazwa)).contains("ul.", "al.", "pl.", "skwer");
-
-        wireMockServer.verify(verifySoapAction(ACTION_POBIERZ_SLOWNIK_CECH_ULIC));
-    }
 
     @Test
     @DisplayName("getUlice(mscId, stanNa) zwraca listę ulic")
