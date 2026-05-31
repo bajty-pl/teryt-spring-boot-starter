d2bbfd3ae1964f080f199e1a2276793aaedc2a1b
Mateusz Polak <mpolak90@gmail.com>
2026-05-31 15:55:57 +0200

    refactor: implement `JednostkaTerytorialna` interface in `Wojewodztwo` and `Gmina`
    - Updated `Wojewodztwo` and `Gmina` models to implement `JednostkaTerytorialna`.
    - Added overridden methods for `id`, `nazwa`, and `poziomJednostkiTerytorialnej`.
    - Enhanced `KodTeryt` interface with a new `value` method.

--- src/main/java/pl/bajty/teryt/model/dto/Gmina.java
+++ src/main/java/pl/bajty/teryt/model/dto/Gmina.java
@@ -1,6 +1,8 @@
 package pl.bajty.teryt.model.dto;
 
+import pl.bajty.teryt.model.enums.PoziomJednostkiTerytorialnej;
 import pl.bajty.teryt.model.enums.RodzajGminy;
+import pl.bajty.teryt.model.interfaces.JednostkaTerytorialna;
 
 import java.time.LocalDate;
 
@@ -9,5 +11,19 @@ public record Gmina(
         String nazwa,
         RodzajGminy rodzajGminy,
         Powiat powiat,
-        LocalDate stanNa) {
+        LocalDate stanNa) implements JednostkaTerytorialna {
+    @Override
+    public Terc id() {
+        return id;
+    }
+
+    @Override
+    public String nazwa() {
+        return nazwa;
+    }
+
+    @Override
+    public PoziomJednostkiTerytorialnej poziomJednostkiTerytorialnej() {
+        return PoziomJednostkiTerytorialnej.GMINA;
+    }
 }
--- src/main/java/pl/bajty/teryt/model/interfaces/KodTeryt.java
+++ src/main/java/pl/bajty/teryt/model/interfaces/KodTeryt.java
@@ -1,4 +1,5 @@
 package pl.bajty.teryt.model.interfaces;
 
 public interface KodTeryt {
+    String value();
 }
